9bbf6e094a29a945be1eaae5b9a2da137d19018c
Connor Ryan <Work1998@hotmail.co.uk>
2021-03-11 15:54:26 +1000

    Removed the help

--- application/config/database.php
+++ application/config/database.php
@@ -1,5 +1,5 @@
 <?php
-defined('BASEPATH') OR exit('No direct script access allowed');
+defined('BASEPATH') or exit('No direct script access allowed');
 
 /*
 | -------------------------------------------------------------------
@@ -76,8 +76,8 @@ $query_builder = TRUE;
 $db['default'] = array(
 	'dsn'	=> '',
 	'hostname' => 'localhost',
-	'username' => 'root',
-	'password' => 'cf87deb87882c2f6',
+	'username' => 'admin',
+	'password' => 'production1',
 	'database' => 'production',
 	'dbdriver' => 'mysqli',
 	'dbprefix' => '',
