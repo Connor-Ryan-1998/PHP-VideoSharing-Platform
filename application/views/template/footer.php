a680ad5bd22c9a89f9eebf725aaf3be071386a18
Connor Ryan <Work1998@hotmail.co.uk>
2021-03-15 20:03:08 +1000

    views update

--- application/views/template/footer.php
+++ application/views/template/footer.php
@@ -1,3 +1,12 @@
-<?php
- //put your code here 
-?>
+<footer>
+    <div class="container">
+        <div class="row vcenter">
+            <div class="col-xs-6">
+                <p>&copy; 2021-<?php echo date("Y"); ?></p>
+            </div>
+        </div>
+    </div>
+</footer>
+</body>
+
+</html>
