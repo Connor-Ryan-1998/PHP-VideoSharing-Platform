374ce8a8d6ff3e5a077ba186b50bb7620239443e
Connor Ryan <Work1998@hotmail.co.uk>
2021-03-15 20:23:50 +1000

    ammending base url

--- application/views/template/header.php
+++ application/views/template/header.php
@@ -1,7 +1,7 @@
 <html>
 
 <head>
-    <title>INFS3202 Demo</title>
+    <title>INFS3202 Video Sharing Platform</title>
     <link rel="stylesheet" type="text/css" href="<?php echo base_url(); ?>assets/css/bootstrap.css">
     <script src="<?php echo base_url(); ?>assets/js/jquery-3.6.0.min.js"></script>
     <script src="<?php echo base_url(); ?>assets/js/bootstrap.js"></script>
@@ -28,7 +28,7 @@
         }
     </script>
     <nav class="navbar navbar-expand-lg navbar-light bg-light">
-        <a class="navbar-brand" href="#">INFS3202 Demo</a>
+        <a class="navbar-brand" href="#">INFS3202 Video Sharing Platform</a>
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
