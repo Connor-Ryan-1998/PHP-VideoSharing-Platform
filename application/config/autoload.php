56bb0be2b9a8ca175667b64885d715dac637163f
Connor Ryan <Work1998@hotmail.co.uk>
2021-03-15 20:15:21 +1000

    controller and login config

--- application/config/autoload.php
+++ application/config/autoload.php
@@ -1,5 +1,5 @@
 <?php
-defined('BASEPATH') OR exit('No direct script access allowed');
+defined('BASEPATH') or exit('No direct script access allowed');
 
 /*
 | -------------------------------------------------------------------
@@ -40,7 +40,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 |
 */
 $autoload['packages'] = array();
-
+$autoload['helper'] = array('url', 'form');
 /*
 | -------------------------------------------------------------------
 |  Auto-load Libraries
