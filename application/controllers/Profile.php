1c7265512f3abef8b168ac50cb3b778759c41aa0
Connor Ryan <Work1998@hotmail.co.uk>
2021-03-17 19:47:03 +1000

    New views and controllers

---
+++ application/controllers/Profile.php
@@ -0,0 +1,38 @@
+<?php
+defined('BASEPATH') or exit('No direct script access allowed');
+
+class Profile extends CI_Controller
+{
+
+    /**
+     * Index Page for this controller.
+     *
+     * Maps to the following URL
+     * 		http://example.com/index.php/welcome
+     *	- or -
+     * 		http://example.com/index.php/welcome/index
+     *	- or -
+     * Since this controller is set as the default controller in
+     * config/routes.php, it's displayed at http://example.com/
+     *
+     * So any other public methods not prefixed with an underscore will
+     * map to /index.php/welcome/<method_name>
+     * @see https://codeigniter.com/user_guide/general/urls.html
+     */
+    public function __construct()
+    {
+        parent::__construct();
+        $this->load->library('session'); //enable session
+    }
+
+    public function mainPage()
+    {
+        $this->load->database();
+        $data['error'] = "";
+        $this->load->helper('form');
+        $this->load->helper('url');
+        $this->load->view('template/header');
+        $this->load->view('Profile', $data);
+        $this->load->view('template/footer');
+    }
+}
---
+++ application/views/Profile.php
@@ -0,0 +1,5 @@
+<div class="container">
+    <div class="col-4 offset-4">
+        <h2 class="text-center">Profile</h2>
+    </div>
+</div>
