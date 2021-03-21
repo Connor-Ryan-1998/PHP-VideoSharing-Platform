c6731b9a19a2d1a3a2ebbb11463b83da5070208d
Connor Ryan <Work1998@hotmail.co.uk>
2021-03-21 22:13:26 +1000

    Test removal of modal

--- application/controllers/Login.php
+++ application/controllers/Login.php
@@ -19,30 +19,30 @@ class login extends CI_Controller
 	}
 	public function check_login()
 	{
-		$this->load->model('user_model');		//load user model
-		$data['error'] = "<div class=\"alert alert-danger\" role=\"alert\"> Incorrect username or passwrod!! </div> ";
-		$this->load->helper('form');
-		$this->load->helper('url');
-		$this->load->view('template/header');
-		$username = $this->input->post('username'); //getting username from login form
-		$password = $this->input->post('password'); //getting password from login form
-		if (!$this->session->userdata('logged_in')) {	//Check if user already login
-			if ($this->user_model->login($username, $password)) //check username and password
-			{
-				$user_data = array(
-					'username' => $username,
-					'logged_in' => true 	//create session variable
-				);
-				$this->session->set_userdata($user_data); //set user status to login in session
-				redirect('login'); // direct user home page
-			} else {
-				$this->load->view('login', $data);	//if username password incorrect, show error msg and ask user to login
-			}
-		} else { {
-				redirect('login'); //if user already logined direct user to home page
-			}
-			$this->load->view('template/footer');
-		}
+		// $this->load->model('user_model');		//load user model
+		// $data['error'] = "<div class=\"alert alert-danger\" role=\"alert\"> Incorrect username or passwrod!! </div> ";
+		// $this->load->helper('form');
+		// $this->load->helper('url');
+		// $this->load->view('template/header');
+		// $username = $this->input->post('username'); //getting username from login form
+		// $password = $this->input->post('password'); //getting password from login form
+		// if (!$this->session->userdata('logged_in')) {	//Check if user already login
+		// 	if ($this->user_model->login($username, $password)) //check username and password
+		// 	{
+		// 		$user_data = array(
+		// 			'username' => $username,
+		// 			'logged_in' => true 	//create session variable
+		// 		);
+		// 		$this->session->set_userdata($user_data); //set user status to login in session
+		// 		redirect('login'); // direct user home page
+		// 	} else {
+		// 		$this->load->view('login', $data);	//if username password incorrect, show error msg and ask user to login
+		// 	}
+		// } else { {
+		// 		redirect('login'); //if user already logined direct user to home page
+		// 	}
+		// 	$this->load->view('template/footer');
+		// }
 	}
 
 	public function logout()
