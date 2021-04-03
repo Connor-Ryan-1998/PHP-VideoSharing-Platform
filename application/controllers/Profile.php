0c69ebaa8a6b753341cf582e0298acb20a826d7f
Connor Ryan <Work1998@hotmail.co.uk>
2021-04-03 15:54:41 +1000

    updating call to index?

--- application/controllers/Profile.php
+++ application/controllers/Profile.php
@@ -3,29 +3,7 @@ defined('BASEPATH') or exit('No direct script access allowed');
 
 class Profile extends CI_Controller
 {
-
-    /**
-     * Index Page for this controller.
-     *
-     * Maps to the following URL
-     * 		http://example.com/index.php/welcome
-     *	- or -
-     * 		http://example.com/index.php/welcome/index
-     *	- or -
-     * Since this controller is set as the default controller in
-     * config/routes.php, it's displayed at http://example.com/
-     *
-     * So any other public methods not prefixed with an underscore will
-     * map to /index.php/welcome/<method_name>
-     * @see https://codeigniter.com/user_guide/general/urls.html
-     */
-    public function __construct()
-    {
-        parent::__construct();
-        $this->load->library('session'); //enable session
-    }
-
-    public function Profile()
+    public function index()
     {
         $this->load->database();
         $data['error'] = "";
@@ -34,5 +12,10 @@ class Profile extends CI_Controller
         $this->load->view('template/header');
         $this->load->view('profile', $data);
         $this->load->view('template/footer');
+    }
+    public function __construct()
+    {
+        parent::__construct();
+        $this->load->library('session'); //enable session
     }
 }
