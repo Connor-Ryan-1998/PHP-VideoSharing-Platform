d83e33a304f118d763c7b9d1908fb25dbc8f08b3
Connor Ryan <Work1998@hotmail.co.uk>
2021-04-03 17:22:43 +1000

    Test push for reg like welcome

--- application/controllers/Registration.php
+++ application/controllers/Registration.php
@@ -5,12 +5,12 @@ class Registration extends CI_Controller
 {
 	public function index()
 	{
-		// $this->load->database();
+		$this->load->database();
 		$data['error'] = "";
-		// $this->load->helper('form');
-		// $this->load->helper('url');
-		// $this->load->view('template/header');
+		$this->load->helper('form');
+		$this->load->helper('url');
+		$this->load->view('template/header');
 		$this->load->view('registration', $data);
-		// $this->load->view('template/footer');
+		$this->load->view('template/footer');
 	}
 }
