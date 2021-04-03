dc8bc3b5240b336b946d023ac6a35500be5381c1
Connor Ryan <Work1998@hotmail.co.uk>
2021-04-03 17:35:25 +1000

    Updating header

--- application/views/template/header.php
+++ application/views/template/header.php
@@ -9,7 +9,7 @@
 
 <body>
     <nav class="navbar navbar-expand-lg navbar-light bg-light">
-        <a class="navbar-brand" href="#">INFS3202 Demo</a>
+        <a class="navbar-brand" href="#">INFS3202 Video Sharing Platform</a>
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
@@ -95,9 +95,5 @@
         <style>
             button.btn.collapsed:before {
                 content: 'Show Result';
-            }
-
-            button.btn:before {
-                content: 'Hide Result';
             }
         </style>
