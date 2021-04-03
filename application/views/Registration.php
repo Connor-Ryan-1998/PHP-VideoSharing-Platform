d1a33103539383741281d876c68a710679a08d4d
Connor Ryan <Work1998@hotmail.co.uk>
2021-04-03 17:32:44 +1000

    registration form setup

--- application/views/Registration.php
+++ application/views/Registration.php
@@ -1,6 +1,6 @@
 <div class="container">
     <div class="col-4 offset-4">
-        <?php echo form_open(base_url() . 'login/check_login'); ?>
+        <?php echo form_open(); ?>
         <h2 class="text-center">Registration</h2>
         <div class="form-group">
             <input type="text" class="form-control" placeholder="Username" required="required" name="username">
