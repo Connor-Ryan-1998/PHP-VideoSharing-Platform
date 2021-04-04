7237f1fbfe5144e42743301a8a148b3f55570106
Connor Ryan <Work1998@hotmail.co.uk>
2021-04-04 12:20:18 +1000

    remove error

--- application/views/profile.php
+++ application/views/profile.php
@@ -1,15 +1,4 @@
 <div class="row justify-content-center">
-    <?php echo form_open_multipart('upload/do_upload'); ?>
-    <div class="col-md-4 col-md-offset-6 centered">
-        <?php echo $error; ?>
-        <div class="form-group">
-            <input type="file" name="userfile" size="20" />
-        </div>
-        <div class="form-group">
-            <input type="submit" value="upload" />
-        </div>
-    </div>
-    <?php echo form_close(); ?>
     <form class="form-inline my-2 my-lg-0">
         <?php echo form_open('ajax'); ?>
         <input class="form-control mr-sm-2" type="search" id="search_text" placeholder="Search" name="search" aria-label="Search">
