fd0b3ced9805a5e224acd2654d9f3fdf303b882c
Connor Ryan <Work1998@hotmail.co.uk>
2021-04-06 14:20:17 +1000

    t

--- application/controllers/Profile.php
+++ application/controllers/Profile.php
@@ -13,6 +13,7 @@ class Profile extends CI_Controller
             $this->load->model('user_model');
             $username = $_SESSION['username'];
             $data = $this->user_model->userData($username);
+            echo "<script>console.log('" . json_encode($data['username']) . "');</script>";
             $this->load->view('profile', $data);
         } else {
             $this->load->view('login');
