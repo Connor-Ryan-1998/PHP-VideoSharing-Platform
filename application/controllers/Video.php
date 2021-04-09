792b928876d1d737badbd6c27864ed65439802be
Connor Ryan <Work1998@hotmail.co.uk>
2021-04-09 23:04:27 +1000

    starting of video config

---
+++ application/controllers/Video.php
@@ -0,0 +1,27 @@
+<?php
+defined('BASEPATH') or exit('No direct script access allowed');
+class Video extends CI_Controller
+{
+    public function index()
+    {
+        $this->load->view('template/header');
+        if (!$this->session->userdata('logged_in')) //check if user already login
+        {
+            if (get_cookie('remember')) { // check if user activate the "remember me" feature  
+                $username = get_cookie('username'); //get the username from cookie
+                $password = get_cookie('password'); //get the username from cookie
+                if ($this->user_model->login($username, $password)) //check username and password correct
+                {
+                    $user_data = array('username' => $username, 'logged_in' => true);
+                    $this->session->set_userdata($user_data); //set user status to login in session
+                    $this->load->view('file', array('error' => ' ')); //if user already logined show upload page
+                }
+            } else {
+                redirect('login'); //if user already logined direct user to home page
+            }
+        } else {
+            $this->load->view('file', array('error' => ' ')); //if user already logined show login page
+        }
+        $this->load->view('template/footer');
+    }
+}
---
+++ application/models/Video_model.php
@@ -0,0 +1,5 @@
+<?php if (!defined('BASEPATH')) exit('No direct script access allowed');
+//put your code here
+class Video_model extends CI_Model
+{
+}
