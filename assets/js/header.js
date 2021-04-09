e725027727e30c662e088a2148675f405a87e1a9
Connor Ryan <Work1998@hotmail.co.uk>
2021-04-09 22:46:54 +1000

    setting up redirect

--- assets/js/header.js
+++ assets/js/header.js
@@ -20,7 +20,51 @@ $(document).ready(function() {
         select: function(event, ui) {
             // Set selection
             $('#search_text').val(ui.item.label); // display the selected text
+            window.location.replace(baseURL+"video/search?query=" + ui.item.id);
             return false;
         }
     });
 });
+
+
+            // load_data();
+
+            // function load_data(query) {
+            //     $.ajax({
+            //         url: "<?php echo base_url(); ?>ajax/fetch",
+            //         method: "GET",
+            //         data: {
+            //             query: query
+            //         },
+            //         success: function(response) {
+            //             $('#result').html("");
+            //             if (response == "") {
+            //                 $('#result').html(response);
+            //             } else {
+            //                 var obj = JSON.parse(response);
+            //                 if (obj.length > 0) {
+            //                     var items = [];
+            //                     $.each(obj, function(i, val) {
+            //                         items.push($("<h4>").text(val.filename));
+            //                         if (val.filename.includes("jpg")) {
+            //                             items.push($('<img width="320" height="240" src="' + '<?php echo base_url(); ?>/uploads/' + val.filename + '" />'));
+            //                         } else {
+            //                             items.push($('<video width="320" height="240" controls><source  src="' + '<?php echo base_url(); ?>/uploads/' + val.filename + '" type="video/mp4"></video>'));
+            //                         }
+            //                     });
+            //                     $('#result').append.apply($('#result'), items);
+            //                 } else {
+            //                     $('#result').html("Not Found!");
+            //                 };
+            //             };
+            //         }
+            //     });
+            // }
+            // $('#search_text').keyup(function() {
+            //     var search = $(this).val();
+            //     if (search != '') {
+            //         load_data(search);
+            //     } else {
+            //         load_data();
+            //     }
+            // });
