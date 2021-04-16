2d080ca6b4203a6e80af17231bd606bec6f6c3a5
Connor Ryan <Work1998@hotmail.co.uk>
2021-04-16 17:21:03 +1000

    b

--- assets/js/video.js
+++ assets/js/video.js
@@ -2,31 +2,28 @@ $(document).ready(function() {
         load_data();
 
         function load_data() {
-            console.log("ruh ro");
             $.ajax({
                 url: baseURL + "ajax/fetchRecent",
                 method: "POST",
                 success: function(data) {
-                    $('#videoList').html("");
-                    if (data == "") {
-                        $('#videoList').html(data);
-                    } else {
-                        var obj = JSON.parse(data);
-                        if (obj.length > 0) {
-                            var items = [];
-                            $.each(obj, function(i, val) {
-                                items.push($("<h4>").text(val.filename));
-                                items.push($('<video width="320" height="240" controls><source  src="' + '<?php echo base_url(); ?>/uploads/' + val.filename + '" type="video/mp4"></video>'));
-                            });
-                            $('#videoList').append.apply($('#videoList'), items);
-                        } else {
-                            $('#videoList').html("Not Found!");
-                        };
-                    };
-                },
-                error: function() {
-                    console.log("ruh ro");
-                 }                
+                    console.log(data);
+                    // $('#videoList').html("");
+                    // if (data == "") {
+                    //     $('#videoList').html(data);
+                    // } else {
+                    //     var obj = JSON.parse(data);
+                    //     if (obj.length > 0) {
+                    //         var items = [];
+                    //         $.each(obj, function(i, val) {
+                    //             items.push($("<h4>").text(val.filename));
+                    //             items.push($('<video width="320" height="240" controls><source  src="' + '<?php echo base_url(); ?>/uploads/' + val.filename + '" type="video/mp4"></video>'));
+                    //         });
+                    //         $('#videoList').append.apply($('#videoList'), items);
+                    //     } else {
+                    //         $('#videoList').html("Not Found!");
+                    //     };
+                    // };
+                },              
             });
         }
 });
