f062a522732edc87273185fa1537d4e625f101ef
Connor Ryan <Work1998@hotmail.co.uk>
2021-04-16 17:21:51 +1000

    aaaa

--- application/controllers/Ajax.php
+++ application/controllers/Ajax.php
@@ -31,7 +31,6 @@ class ajax extends CI_Controller
     {
         $this->load->model('file_model');
         $data = $this->file_model->fetchRecentlyUploaded();
-        // echo json_encode($data->result());
-        return $data->result();
+        echo json_encode($data->result());
     }
 }
