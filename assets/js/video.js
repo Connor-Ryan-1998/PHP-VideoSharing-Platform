4a6ebd8ecba5878271678dec3d23007f0accf58a
Connor Ryan <Work1998@hotmail.co.uk>
2021-04-16 18:03:34 +1000

    ?

--- assets/js/video.js
+++ assets/js/video.js
@@ -7,22 +7,22 @@ $(document).ready(function() {
                 method: "GET",
                 success: function(data) {
                     console.log(data);
-                    // $('#videoList').html("");
-                    // if (data == "") {
-                    //     $('#videoList').html(data);
-                    // } else {
-                    //     var obj = JSON.parse(data);
-                    //     if (obj.length > 0) {
-                    //         var items = [];
-                    //         $.each(obj, function(i, val) {
-                    //             items.push($("<h4>").text(val.filename));
-                    //             items.push($('<video width="320" height="240" controls><source  src="' + '<?php echo base_url(); ?>/uploads/' + val.filename + '" type="video/mp4"></video>'));
-                    //         });
-                    //         $('#videoList').append.apply($('#videoList'), items);
-                    //     } else {
-                    //         $('#videoList').html("Not Found!");
-                    //     };
-                    // };
+                    $('#videoList').html("");
+                    if (data == "") {
+                        $('#videoList').html(data);
+                    } else {
+                        var obj = JSON.parse(data);
+                        if (obj.length > 0) {
+                            var items = [];
+                            $.each(obj, function(i, val) {
+                                items.push($("<h4>").text(val.filename));
+                                items.push($('<video width="320" height="240" controls><source  src="' + '<?php echo base_url(); ?>/uploads/' + val.filename + '" type="video/mp4"></video>'));
+                            });
+                            $('#videoList').append.apply($('#videoList'), items);
+                        } else {
+                            $('#videoList').html("Not Found!");
+                        };
+                    };
                 },              
             });
         }
