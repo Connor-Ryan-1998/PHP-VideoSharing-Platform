c547589f2646bb20c3de9f4332d52a0c44d54962
Connor Ryan <Work1998@hotmail.co.uk>
2021-04-17 16:13:38 +1000

    a

--- application/views/profile.php
+++ application/views/profile.php
@@ -6,7 +6,6 @@
                 <?php echo form_open_multipart('upload/do_upload'); ?>
                 <div class="row justify-content-center">
                     <div class="col-md-4 col-md-offset-6 centered">
-                        <?php echo $error; ?>
                         <div class="form-group">
                             <input type="file" name="userfile" size="20" />
                         </div>
