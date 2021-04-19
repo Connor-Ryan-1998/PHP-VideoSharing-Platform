25a274aff5af2d93cf4060758bf004cdabd6b03a
Connor Ryan <Work1998@hotmail.co.uk>
2021-04-19 23:55:02 +1000

    echo registration pw

--- application/models/Register_model.php
+++ application/models/Register_model.php
@@ -5,13 +5,13 @@ class Register_model extends CI_Model
     {
         ///Hash Password
         $password = password_hash($password, PASSWORD_DEFAULT);
-
+        echo "<script>console.log('Debug Objects: {$password}' );</script>";
         //Get Max id
         $this->db->select_max('id');
         $result = $this->db->get('users')->result_array();
         foreach ($result as $row) {
             $data = array(
-                'id' => $row['id'],
+                'id' => $row['id'] + 1,
                 'username' => $username,
                 'emailAddress' => $emailAddress,
                 'password' => $password
