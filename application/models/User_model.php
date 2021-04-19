33e8f4c177a2c4427bf5fc6331a0c715855ebef4
Connor Ryan <Work1998@hotmail.co.uk>
2021-04-19 23:01:19 +1000

    a

--- application/models/User_model.php
+++ application/models/User_model.php
@@ -11,7 +11,7 @@ class User_model extends CI_Model
         $result = $this->db->get('users')->result_array();
         foreach ($result as $row) {
             $value = password_hash($password, PASSWORD_DEFAULT);
-            echo "<script>console.log('Debug Objects: {$value},{$row['password']}' );</script>";
+            echo "<script>console.log('Debug Objects: {$value}   {$row['password']}' );</script>";
             if (password_verify($password, $row['password'])) {
                 return true;
             } else {
