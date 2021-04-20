71da4c7469e35037de586dbc4d6f0d23ea9b2e80
Connor Ryan <Work1998@hotmail.co.uk>
2021-04-20 22:45:35 +1000

    aa

--- application/models/Register_model.php
+++ application/models/Register_model.php
@@ -47,5 +47,8 @@ class Register_model extends CI_Model
         $this->email->subject('PHP-VideoSharing-Platform Email Verification');
         $this->email->message($message);
         $this->email->send();
+        $this->load->view('template/header');
+        $this->load->view('login');
+        $this->load->view('template/footer');
     }
 }
