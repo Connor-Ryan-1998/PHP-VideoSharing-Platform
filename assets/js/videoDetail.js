76459d8c451af6cdc21980ed84db5c39b2ebff51
Connor Ryan <Work1998@hotmail.co.uk>
2021-04-21 23:26:05 +1000

    aaa

--- assets/js/videoDetail.js
+++ assets/js/videoDetail.js
@@ -2,31 +2,31 @@ $(document).ready(function() {
         load_data();
         load_Comments();
 
-        function load_data() {
-            $.ajax({
-                url: baseURL + "ajax/fetchVideoDetail",
-                method: "GET",
-                success: function(data) {
-                    $('#videoDetail').html("");
-                    if (data == "") {
-                        $('#videoDetail').html(data);
-                    } else {
-                        var obj = JSON.parse(data);
-                        if (obj.length > 0) {
-                            var items = [];
-                            $.each(obj, function(i, val) {
-                                items.push($("<h4>").text(val.filename));
-                                items.push($('<video width="320" height="240" controls><source  src="' + baseURL+'uploads/' + val.filename + '" type="video/mp4"></video>'));
-                                items.push($("<h4>").text(val.description));
-                            });
-                            $('#videoDetail').append.apply($('#videoDetail'), items);
-                        } else {
-                            $('#videoDetail').html("Not Found!");
-                        };
-                    };
-                },              
-            });
-        }
+        // function load_data() {
+        //     $.ajax({
+        //         url: baseURL + "ajax/fetchVideoDetail",
+        //         method: "GET",
+        //         success: function(data) {
+        //             $('#videoDetail').html("");
+        //             if (data == "") {
+        //                 $('#videoDetail').html(data);
+        //             } else {
+        //                 var obj = JSON.parse(data);
+        //                 if (obj.length > 0) {
+        //                     var items = [];
+        //                     $.each(obj, function(i, val) {
+        //                         items.push($("<h4>").text(val.filename));
+        //                         items.push($('<video width="320" height="240" controls><source  src="' + baseURL+'uploads/' + val.filename + '" type="video/mp4"></video>'));
+        //                         items.push($("<h4>").text(val.description));
+        //                     });
+        //                     $('#videoDetail').append.apply($('#videoDetail'), items);
+        //                 } else {
+        //                     $('#videoDetail').html("Not Found!");
+        //                 };
+        //             };
+        //         },              
+        //     });
+        // }
         function load_Comments() {
             $.ajax({
                 url: baseURL + "ajax/fetchVideoDetailComments",
