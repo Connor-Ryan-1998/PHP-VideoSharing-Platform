121ea430d06c72e5bd30decfcdd0581cf7df0de0
Connor Ryan <Work1998@hotmail.co.uk>
2021-04-22 21:53:08 +1000

    full bottom?

--- application/views/template/footer.php
+++ application/views/template/footer.php
@@ -1,5 +1,5 @@
 <footer>
-    <div class="container bg-dark text-white">
+    <div class="container bg-dark text-white w-100">
         <div class="row vcenter">
             <div class="col-xs-6">
                 <p> Connor Ryan S4434200 &copy; : 2021-<?php echo date("Y"); ?></p>
