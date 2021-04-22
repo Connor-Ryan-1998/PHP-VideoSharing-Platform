3b2b206b0cc47c60d42ff43c610aee4052fd8873
Connor Ryan <Work1998@hotmail.co.uk>
2021-04-22 21:01:06 +1000

    remove unecessary button

--- application/views/template/header.php
+++ application/views/template/header.php
@@ -49,7 +49,6 @@
                 <form class="form-inline my-2 my-lg-0 mx-auto">
                     <?php echo form_open('ajax'); ?>
                     <input class="form-control mr-sm-2" type="search" id="search_text" placeholder="Search" name="search" aria-label="Search">
-                    <button id="resultButton" class="btn btn-outline-success my-2 my-sm-0" type="button" data-toggle="collapse" data-target="#collapseExample" aria-expanded="false" aria-controls="collapseExample"> </button>
                     <?php echo form_close(); ?>
             </ul>
         </div>
