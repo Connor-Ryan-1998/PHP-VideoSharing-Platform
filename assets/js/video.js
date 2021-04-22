85abeba8638d5dd6a1b30ef6dc3453f80f04b1d0
Connor Ryan <Work1998@hotmail.co.uk>
2021-04-22 21:31:01 +1000

    a

--- assets/js/video.js
+++ assets/js/video.js
@@ -14,7 +14,7 @@ $(document).ready(function() {
                         if (obj.length > 0) {
                             var items = [];
                             $.each(obj, function(i, val) {
-                                items.push($("<h4><a href="+ baseURL+"video/SearchVideo/" + val.id+" class=" + text(text-decoration-none) + ">"+val.filename+"</a></h4>"));
+                                items.push($("<h4><a href="+ baseURL+"video/SearchVideo/" + val.id+" class=\"text-decoration-none\">"+val.filename+"</a></h4>"));
                                 items.push($('<video width="320" height="240" controls><source  src="' + baseURL+'uploads/' + val.filename + '" type="video/mp4"></video>'));
                             });
                             $('#videoList').append.apply($('#videoList'), items);
