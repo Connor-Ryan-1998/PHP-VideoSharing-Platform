e83168bddf3ce1796cefaf6b0ecd909592b334f0
Connor Ryan <Work1998@hotmail.co.uk>
2021-05-04 11:11:01 +1000

    wrap outside of column

--- application/views/profile.php
+++ application/views/profile.php
@@ -55,22 +55,22 @@
             <div class="col-sm">
                 <h2 class="text-center">Additional Data</h2>
                 <a href="<?php echo base_url(); ?>managementDashboard" class="btn btn-dark btn-info" role="button">Management Dashboard</a>
-                <button class="open-button" onclick="openForm()">Chat</button>
-                <!-- Chat bot to assist user -->
-                <div class="chat-popup" id="myForm">
-                    <form action="/action_page.php" class="form-container">
-                        <h1>Chat</h1>
-
-                        <label for="msg"><b>Message</b></label>
-                        <textarea placeholder="Type message.." name="msg" required></textarea>
-
-                        <button type="submit" class="btn">Send</button>
-                        <button type="button" class="btn cancel" onclick="closeForm()">Close</button>
-                    </form>
-                </div>
             </div>
         </div>
     </div>
+</div>
+<button class="open-button" onclick="openForm()">Chat</button>
+<!-- Chat bot to assist user -->
+<div class="chat-popup" id="myForm">
+    <form action="/action_page.php" class="form-container">
+        <h1>Chat</h1>
+
+        <label for="msg"><b>Message</b></label>
+        <textarea placeholder="Type message.." name="msg" required></textarea>
+
+        <button type="submit" class="btn">Send</button>
+        <button type="button" class="btn cancel" onclick="closeForm()">Close</button>
+    </form>
 </div>
 <script>
     function openForm() {
