8e07730d53e7d464bd7a50814c1c32d8b61a6537
Connor Ryan <Work1998@hotmail.co.uk>
2021-05-04 16:48:21 +1000

    ahahahahahahahahah

--- assets/js/footer.js
+++ assets/js/footer.js
@@ -5,10 +5,19 @@ $(document).ready(function() {
 
     $("#submitBotQuestion").click(function(e) {
         const mainDiv = document.getElementById("chatBotChat");
+
+        //Creates your Question to be submitted
         let userDiv = document.createElement("div");
         userDiv.id = "userChat";
         var input = $("#chatBotInput").val();
         userDiv.innerHTML = `You: <span id="user-response">${input}</span>`;
+
+
+        ///Creates Bot to be submitted
+        let botDiv = document.createElement("div");
+        botDiv.id = "botChat";
+        var input = $("#chatBotInput").val();
+        userDiv.innerHTML = `You: <span id="bot-response">${input}</span>`;
         if (input != ""){
             mainDiv.appendChild(userDiv);
         }
