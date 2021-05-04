5b639812671c65384e46d6dd2efa39605a3277c4
Connor Ryan <Work1998@hotmail.co.uk>
2021-05-04 18:12:17 +1000

    test

--- assets/js/footer.js
+++ assets/js/footer.js
@@ -39,8 +39,8 @@ $(document).ready(function() {
             },
             dataType: "JSON",
             success: function(data) {
-                var obj = JSON.parse(data[0]);
-                console.log(obj);
+                // var obj = JSON.parse(data);
+                console.log(data[0]);
                 return data;              
             }             
         });
