f52da53dfa0893d809e7449ca9790ec4e522762d
Connor Ryan <Work1998@hotmail.co.uk>
2021-05-04 14:51:25 +1000

    aask

--- assets/js/footer.js
+++ assets/js/footer.js
@@ -2,9 +2,9 @@ document.addEventListener("DOMContentLoaded", () => {
     const inputField = document.getElementById("chatBotInput")
     inputField.addEventListener("keydown", function(e) {
         if (e.code === "Enter") {
-            return false;
             let input = inputField.value;
             console.log(`I typed '${input}'`)
+            return false;
         }
     });
 });
