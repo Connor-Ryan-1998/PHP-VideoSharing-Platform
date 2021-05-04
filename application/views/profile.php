7b96d1e301b2b39fce915869bbc8928d2e5bf7df
Connor Ryan <Work1998@hotmail.co.uk>
2021-05-04 11:13:19 +1000

    try sit it

--- application/views/profile.php
+++ application/views/profile.php
@@ -58,19 +58,19 @@
             </div>
         </div>
     </div>
-</div>
-<button class="open-button" onclick="openForm()">Chat</button>
-<!-- Chat bot to assist user -->
-<div class="chat-popup" id="myForm">
-    <form action="/action_page.php" class="form-container">
-        <h1>Chat</h1>
+    <button class="open-button" onclick="openForm()">Chat</button>
+    <!-- Chat bot to assist user -->
+    <div class="chat-popup" id="myForm">
+        <form action="/action_page.php" class="form-container">
+            <h1>Chat</h1>
 
-        <label for="msg"><b>Message</b></label>
-        <textarea placeholder="Type message.." name="msg" required></textarea>
+            <label for="msg"><b>Message</b></label>
+            <textarea placeholder="Type message.." name="msg" required></textarea>
 
-        <button type="submit" class="btn">Send</button>
-        <button type="button" class="btn cancel" onclick="closeForm()">Close</button>
-    </form>
+            <button type="submit" class="btn">Send</button>
+            <button type="button" class="btn cancel" onclick="closeForm()">Close</button>
+        </form>
+    </div>
 </div>
 <script>
     function openForm() {
