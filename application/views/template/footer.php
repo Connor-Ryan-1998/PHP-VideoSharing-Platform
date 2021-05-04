24c22cf0eefdc70f6e1a3d8160dae8cf0e9c55eb
Connor Ryan <Work1998@hotmail.co.uk>
2021-05-04 11:52:42 +1000

    test radius

--- application/views/template/footer.php
+++ application/views/template/footer.php
@@ -8,7 +8,6 @@
         <textarea placeholder="Type message.." name="msg" required></textarea>
 
         <button type="submit" class="btn">Send</button>
-        <button type="button" class="btn cancel" onclick="closeForm()">Close</button>
     </form>
 </div>
 <footer>
@@ -16,7 +15,7 @@
         <div class="row vcenter">
             <div class="col-xs-6">
                 <p> Connor Ryan S4434200 &copy; : 2021-<?php echo date("Y"); ?></p>
-                <button class="btn btn-info btn-circle btn-xl" onclick="toggleFormState()">Chatbot</button>
+                <button class="open-button " onclick="toggleFormState()">Chatbot</button>
             </div>
         </div>
     </div>
