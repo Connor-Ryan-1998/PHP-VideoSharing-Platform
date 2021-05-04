b5a3094dbce0c9ec4c9c2bc1705f394a267a5fc6
Connor Ryan <Work1998@hotmail.co.uk>
2021-05-04 11:03:42 +1000

    wrap into footer

--- application/views/template/footer.php
+++ application/views/template/footer.php
@@ -5,8 +5,30 @@
             <div class="col-xs-6">
                 <p> Connor Ryan S4434200 &copy; : 2021-<?php echo date("Y"); ?></p>
             </div>
+            <button class="open-button" onclick="openForm()">Chat</button>
+            <div class="chat-popup" id="myForm">
+                <form action="/action_page.php" class="form-container">
+                    <h1>Chat</h1>
+
+                    <label for="msg"><b>Message</b></label>
+                    <textarea placeholder="Type message.." name="msg" required></textarea>
+
+                    <button type="submit" class="btn">Send</button>
+                    <button type="button" class="btn cancel" onclick="closeForm()">Close</button>
+                </form>
+            </div>
         </div>
     </div>
 </footer>
+
+<script>
+    function openForm() {
+        document.getElementById("myForm").style.display = "block";
+    }
+
+    function closeForm() {
+        document.getElementById("myForm").style.display = "none";
+    }
+</script>
 
 </html>
--- application/views/video.php
+++ application/views/video.php
@@ -15,17 +15,6 @@
                 </div>
             </div>
             <div class="col-sm">
-                <div class="chat-popup" id="myForm">
-                    <form action="/action_page.php" class="form-container">
-                        <h1>Chat</h1>
-
-                        <label for="msg"><b>Message</b></label>
-                        <textarea placeholder="Type message.." name="msg" required></textarea>
-
-                        <button type="submit" class="btn">Send</button>
-                        <button type="button" class="btn cancel" onclick="closeForm()">Close</button>
-                    </form>
-                </div>
             </div>
         </div>
     </div>
